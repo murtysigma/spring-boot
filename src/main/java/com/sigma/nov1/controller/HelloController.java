06b4351d3e62526bb6a47d545a875b572122a8cb
klmurty5023 <kirlampalli.murthy@gmail.com>
2022-11-09 21:25:39 +0530

    Add the student controller

---
+++ src/main/java/com/sigma/nov1/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.sigma.nov1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/sayHello")
+	public void sayHello() {
+		
+	}
+}
